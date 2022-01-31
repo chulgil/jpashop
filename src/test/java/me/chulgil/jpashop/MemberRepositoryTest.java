e63c5a286a138288a84e8775cb9e6a6a484c230a
cglee <contact@chulgil.me>
2022-02-01 00:55:49 +0900

    Test: 멤버 테스트코드 추가

--- src/test/java/me/chulgil/jpashop/MemberRepositoryTest.java
+++ src/test/java/me/chulgil/jpashop/MemberRepositoryTest.java
@@ -1,4 +1,42 @@
+package me.chulgil.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class MemberRepositoryTest {
-  
+
+    @Autowired MemberRepository memberRepository;
+
+    /**
+     * @throws Exception
+     */
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
+        // 영속성 컨테이너에 이미 저장된 ID로 저장하기때문에 결과값이 같아야한다.
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+    
 }
